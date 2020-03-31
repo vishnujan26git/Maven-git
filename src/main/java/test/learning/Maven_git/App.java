6794bd08e3b30c7473d1d81dfad3e19e08ccc867
Vishnu <Vishnuswamy5391@gmail.com>
2020-03-31 23:09:18 +0530

    first commit

---
+++ src/main/java/test/learning/Maven_git/App.java
@@ -0,0 +1,13 @@
+package test.learning.Maven_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
